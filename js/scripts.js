749499ecd1c30b50466cfebfa6545799f78a0b32
Taylor Phillips <taylorphillips133@gmail.com>
2020-04-06 10:53:43 -0700

    add our delete contact function and adjusted the find contact function to coalesce

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //  Business Logic for Address Book --------------
 function AddressBook() {
-  this.contacts = []
+  this.contacts = [];
   this.currentId = 0;
 }
 
@@ -11,16 +11,25 @@ AddressBook.prototype.addContact = function(contact) {
 
 AddressBook.prototype.findContact = function(id){
   for (var i = 0; i < this.contacts.length; i++) {
-    if (this.contacts[i].id == id) {
-      return this.contacts[i];
+    if (this.contacts[i]){
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+     }
     }
   };
   return false;
 }
 
-AddressBook.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
+AddressBook.prototype.deleteContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+    if (this.contacts[i].id == id) {
+      delete this.contacts[i];
+      return true;
+    }
+   }
+  };
+  return false;
 }
 
 //  Business Logic for Address Book -------------
