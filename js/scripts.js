d71abc03ee6dc66cbbeacbcb79b528ddc97cc585
Taylor Phillips <taylorphillips133@gmail.com>
2020-04-07 11:19:01 -0700

    add function to findContacts by index location within our AddressBook

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,22 @@ function AddressBook() {
 }
 
 AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
   this.contacts.push(contact);
 }
 
 AddressBook.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+  for (var i = 0; i < this.contacts.length; i++) {
+    if (this.contacts[i].id === id) {
+      return this.contacts[i];
+      }
+    };
+  return false;
 }
 
 // Business Logic for Contacts ---------
